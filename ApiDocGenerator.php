0da57df9869257ba054eeefeadc17692ee7c746b
lincanbin <lincanbin@hotmail.com>
2018-03-24 23:22:21 +0800

    apiDoc generator.

---
+++ ApiDocGenerator.php
@@ -0,0 +1,62 @@
+<?php
+require_once __DIR__ . '/ApiDocCommentObject.php';
+
+/**
+ *
+ */
+class ApiDocGenerator
+{
+    private $input;
+    private $output;
+    private $template;
+    /**
+     * @var RecursiveIteratorIterator $fileIteratorfileIterator
+     */
+    private $fileIterator;
+
+    public function __construct($input, $output, $template)
+    {
+        $this->input = $input;
+        $this->output = $output;
+        $this->template = $template;
+        $this->fileIterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->input));
+
+        foreach ($this->fileIterator as $file) {
+            /**
+             * @var SplFileInfo $file
+             */
+            if ($file->isDir() || $file->getExtension() !== 'php') {
+                continue;
+            }
+            $this->parseFile($file);
+        }
+    }
+
+    private function parseFile(SplFileInfo $splFileInfo)
+    {
+        $fileSize = $splFileInfo->getSize();
+        $fileName = mb_substr($splFileInfo->getPathname(), mb_strlen($this->input) - 1);
+        if ($fileSize === 0 || $fileSize === false) {
+            return false;
+        }
+        echo $splFileInfo->getPathname() . "\n";
+        $splFileObject = $splFileInfo->openFile('r');
+        $docComments = array_filter(token_get_all($splFileObject->fread($fileSize)), function ($entry) {
+            return is_array($entry) && $entry[0] == T_DOC_COMMENT;
+        });
+        foreach ($docComments as $docComment) {
+            $this->parseComment($docComment, $fileName);
+        }
+        return true;
+    }
+
+    private function parseComment($comment, $fileName)
+    {
+        $comment[1] = mb_substr($comment[1], 2, mb_strlen($comment[1]) - 2);
+        echo $fileName . "\n";
+        var_dump($comment);
+        $apiDocCommentObject = new ApiDocCommentObject($comment[1], $fileName, $comment[2]);
+        var_dump($apiDocCommentObject);
+
+    }
+}
