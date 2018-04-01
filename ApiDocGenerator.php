d0cfd511f699c2ea0cf13e577ea4e7c863ff6568
lincanbin <lincanbin@hotmail.com>
2018-04-01 10:32:01 +0800

    Save apiDoc to file.

--- ApiDocGenerator.php
+++ ApiDocGenerator.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/ApiDocCommentObject.php';
+require_once __DIR__ . '/ApiDocConfig.php';
 
 /**
  *
@@ -9,6 +10,9 @@ class ApiDocGenerator
     private $input;
     private $output;
     private $template;
+    protected $config;
+
+    private $apiList;
     /**
      * @var RecursiveIteratorIterator $fileIteratorfileIterator
      */
@@ -16,26 +20,33 @@ class ApiDocGenerator
 
     public function __construct($input, $output, $template)
     {
-        $this->input = $input;
-        $this->output = $output;
-        $this->template = $template;
-        $this->fileIterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->input));
+        try {
+            $this->input = $input;
+            $this->output = $output;
+            $this->template = $template;
+            $this->fileIterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($this->input));
+            $this->config = new ApiDocConfig();
+            $this->config->load($this->input);
 
-        foreach ($this->fileIterator as $file) {
-            /**
-             * @var SplFileInfo $file
-             */
-            if ($file->isDir() || $file->getExtension() !== 'php') {
-                continue;
+            foreach ($this->fileIterator as $file) {
+                /**
+                 * @var SplFileInfo $file
+                 */
+                if ($file->isDir() || $file->getExtension() !== 'php') {
+                    continue;
+                }
+                $this->parseFile($file);
             }
-            $this->parseFile($file);
+        } catch (Exception $exception) {
+            echo $exception->getMessage();
         }
+        var_dump($this->config);
     }
 
     private function parseFile(SplFileInfo $splFileInfo)
     {
         $fileSize = $splFileInfo->getSize();
-        $fileName = mb_substr($splFileInfo->getPathname(), mb_strlen($this->input) - 1);
+        $fileName = str_replace('\\', '/', mb_substr($splFileInfo->getPathname(), mb_strlen($this->input) - 1));
         if ($fileSize === 0 || $fileSize === false) {
             return false;
         }
@@ -57,7 +68,40 @@ class ApiDocGenerator
         echo $fileName . "\n";
         //var_dump($comment);
         $apiDocCommentObject = new ApiDocCommentObject($comment[1], $fileName, $comment[2]);
+
+        $this->saveDoc($apiDocCommentObject);
+    }
+
+    private function saveDoc(ApiDocCommentObject $apidoc)
+    {
         //var_dump($apiDocCommentObject);
-        var_dump($apiDocCommentObject->parsedParams);
+        var_dump($apidoc->parsedParams);
+        if ($apidoc->type === 'api') {
+            $fileName = 'apidoc/' . $apidoc->apiGroup['name'] . '/' . $apidoc->apiName['name'] . '.md';
+        } elseif ($apidoc->type === 'define') {
+            $fileName = 'apidoc/define/' . $apidoc->apiDefine['name'] . '.md';
+        } else {
+            return false;
+        }
+        var_dump($fileName);
+        ob_start();
+        include $this->template . 'apiTemplate.php';
+        $content = ob_get_contents();
+        ob_end_clean();
+        $this->saveFile($fileName, $content);
+        return true;
+    }
+
+    private function saveFile($fileName, $content)
+    {
+        $fileName = $this->output . $fileName;
+        $path = dirname($fileName);
+        if (!is_dir($path)) {
+            mkdir($path, 0775, true);
+        }
+        $handle = fopen($fileName, 'w');
+        $writeResult = fwrite($handle, $content);
+        fclose($handle);
+        return $writeResult;
     }
 }
