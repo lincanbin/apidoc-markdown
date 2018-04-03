19e13ec594ecbe069d83423990e28f6e4c99e42b
lincanbin <lincanbin@hotmail.com>
2018-04-03 23:28:40 +0800

    API template.

--- template/apiTemplate.php
+++ template/apiTemplate.php
@@ -1,52 +1,52 @@
 <?php
 /**
  * @var ApiDocGenerator $this
- * @var ApiDocCommentObject $apidoc
+ * @var ApiDocCommentObject $apiDoc
  */
 
-if ($apidoc->type === 'api'):
+if ($apiDoc->type === 'api'):
     // Title
 ?>
 # <?php
-    echo (!is_null($apidoc->apiDeprecated) ? '~~' : '') . (!is_null($apidoc->api['title']) ? $apidoc->api['title'] : $apidoc->apiName['name']) . (!is_null($apidoc->apiDeprecated) ? '~~' : '');
-    ?> ([<?php echo $apidoc->apiGroup['name']; ?>](../../README.md#<?php echo $apidoc->apiGroup['name']; ?>))
+    echo (!is_null($apiDoc->apiDeprecated) ? '~~' : '') . (!is_null($apiDoc->api['title']) ? $apiDoc->api['title'] : $apiDoc->apiName['name']) . (!is_null($apiDoc->apiDeprecated) ? '~~' : '');
+    ?> ([<?php echo $apiDoc->apiGroup['name']; ?>](../../README.md#<?php echo $apiDoc->apiGroup['name']; ?>))
 <?php
-elseif ($apidoc->type === 'define'):
+elseif ($apiDoc->type === 'define'):
     // apiDefine
 ?>
-# <?php echo (!is_null($apidoc->apiDeprecated) ? '~~' : '') . $apidoc->apiDefine['name'] . (!is_null($apidoc->apiDeprecated) ? '~~' : ''); ?> ([<?php echo $this->config->name ?>](../../README.md#define))
+# <?php echo (!is_null($apiDoc->apiDeprecated) ? '~~' : '') . $apiDoc->apiDefine['name'] . (!is_null($apiDoc->apiDeprecated) ? '~~' : ''); ?> ([<?php echo $this->config->name ?>](../../README.md#define))
 <?php
 endif;
 ?>
 <?php
 // apiDescription
-if (!is_null($apidoc->apiDescription['text'])) :
-    echo $apidoc->apiDescription['text'] . "\n";
-    echo !is_null($apidoc->apiDeprecated['text']) ? " ### Deprecated \n\n> " . str_replace("\n", "\n> ", $apidoc->apiDeprecated['text']) . "\n\n" : '';
+if (!is_null($apiDoc->apiDescription['text'])) :
+    echo $apiDoc->apiDescription['text'] . "\n";
+    echo !is_null($apiDoc->apiDeprecated['text']) ? " ### Deprecated \n\n> " . str_replace("\n", "\n> ", $apiDoc->apiDeprecated['text']) . "\n\n" : '';
 endif;
-if ($apidoc->type === 'api'):
+if ($apiDoc->type === 'api'):
 ?>
 
-`<?php echo strtoupper(substr($apidoc->api['method'], 1, -1)); ?>`
+`<?php echo strtoupper(substr($apiDoc->api['method'], 1, -1)); ?>`
 
 ```
-<?php echo $this->config->url . $apidoc->api['path']; ?>
+<?php echo $this->config->url . $apiDoc->api['path']; ?>
 
 ```
 <?php
 endif;
 
 // apiSampleRequest
-if ($apidoc->type === 'api' && (!is_null($apidoc->apiSampleRequest) || $this->config->sampleUrl !== '')):
+if ($apiDoc->type === 'api' && (!is_null($apiDoc->apiSampleRequest) || $this->config->sampleUrl !== '')):
     ?>
 ##### Sample Request
 
 ```
 <?php
-    if (!is_null($apidoc->apiSampleRequest)) {
-        echo $apidoc->apiSampleRequest['url'];
+    if (!is_null($apiDoc->apiSampleRequest)) {
+        echo $apiDoc->apiSampleRequest['url'];
     } else {
-        echo $this->config->sampleUrl . $apidoc->api['path'];
+        echo $this->config->sampleUrl . $apiDoc->api['path'];
     }
 ?>
 
@@ -56,26 +56,26 @@ if ($apidoc->type === 'api' && (!is_null($apidoc->apiSampleRequest) || $this->co
 endif;
 
 // apiExample
-if (!is_null($apidoc->apiExample)):
+if (!is_null($apiDoc->apiExample)):
     ?>
 ##### Example for usage of an API method
-<?php echo $apidoc->apiExample['title'] ?: ''; ?>
+<?php echo $apiDoc->apiExample['title'] ?: ''; ?>
 
-```<?php echo isset($apidoc->apiExample['type']) ? substr($apidoc->apiExample['type'], 1, -1) : ''; ?>
-<?php echo $apidoc->apiExample['example']; ?>
+```<?php echo isset($apiDoc->apiExample['type']) ? substr($apiDoc->apiExample['type'], 1, -1) : ''; ?>
+<?php echo $apiDoc->apiExample['example']; ?>
 ```
 <?php
 endif;
 
 // apiParam
-if (!is_null($apidoc->apiParam)):
+if (!is_null($apiDoc->apiParam)):
 ?>
 ###  Parameters
 Key|Value Type|Required|Default Value|Description
 ---|---|---|---|---
 <?php
-foreach ($apidoc->apiParam as $param):
-    $parsedParam = $apidoc->parseField($param);
+foreach ($apiDoc->apiParam as $param):
+    $parsedParam = $apiDoc->parseField($param);
 ?>
 <?php
     echo $parsedParam['key'];
@@ -97,26 +97,68 @@ endif;
 ?>
 <?php
 // apiParamExample
-if (!is_null($apidoc->apiParamExample)):
+if (!is_null($apiDoc->apiParamExample)):
 ?>
 ### Parameter Request Example
-<?php echo $apidoc->apiParamExample['title'] ?: ''; ?>
+<?php echo $apiDoc->apiParamExample['title'] ?: ''; ?>
 
-```<?php echo isset($apidoc->apiParamExample['type']) ? substr($apidoc->apiParamExample['type'], 1, -1) : ''; ?>
-<?php echo $apidoc->apiParamExample['example']; ?>
+```<?php echo isset($apiDoc->apiParamExample['type']) ? substr($apiDoc->apiParamExample['type'], 1, -1) : ''; ?>
+<?php echo $apiDoc->apiParamExample['example']; ?>
 ```
 <?php
 endif;
+
+// apiHeader
+if (!is_null($apiDoc->apiHeader)):
+    ?>
+### Parameters passed to you API-Header
+Key|Value Type|Required|Default Value|Description
+---|---|---|---|---
+    <?php
+    foreach ($apiDoc->apiHeader as $param):
+        $parsedParam = $apiDoc->parseField($param);
+        ?>
+        <?php
+        echo $parsedParam['key'];
+        ?>|<?php
+        echo $parsedParam['type'];
+        ?>|<?php
+        echo $parsedParam['required'];
+        ?>|<?php
+        echo $parsedParam['default'];
+        ?>|<?php
+        echo $parsedParam['description'];
+        ?>
+<?php
+    endforeach;?>
+
+<?php
+endif;
+?>
+<?php
+// apiHeaderExample
+if (!is_null($apiDoc->apiHeaderExample)):
+    ?>
+### Parameter request example
+<?php echo $apiDoc->apiHeaderExample['title'] ?: ''; ?>
+
+```<?php echo isset($apiDoc->apiHeaderExample['type']) ? substr($apiDoc->apiHeaderExample['type'], 1, -1) : ''; ?>
+<?php echo $apiDoc->apiHeaderExample['example']; ?>
+```
+<?php
+endif;
+
+
 // apiSuccess
-if (!is_null($apidoc->apiSuccess)):
+if (!is_null($apiDoc->apiSuccess)):
     ?>
 
 ###  Success return Parameter
 Key|Value Type|Description
 ---|---|---
     <?php
-    foreach ($apidoc->apiSuccess as $param):
-        $parsedParam = $apidoc->parseField($param);
+    foreach ($apiDoc->apiSuccess as $param):
+        $parsedParam = $apiDoc->parseField($param);
         ?>
 <?php
 echo $parsedParam['key'];
@@ -134,13 +176,50 @@ endif;
 ?>
 <?php
 // apiSuccessExample
-if (!is_null($apidoc->apiSuccessExample)):
+if (!is_null($apiDoc->apiSuccessExample)):
     ?>
 ### Example of a success return message
-<?php echo $apidoc->apiSuccessExample['title'] ?: ''; ?>
+<?php echo $apiDoc->apiSuccessExample['title'] ?: ''; ?>
 
-```<?php echo isset($apidoc->apiSuccessExample['type']) ? substr($apidoc->apiSuccessExample['type'], 1, -1) : ''; ?>
-<?php echo $apidoc->apiSuccessExample['example']; ?>
+```<?php echo isset($apiDoc->apiSuccessExample['type']) ? substr($apiDoc->apiSuccessExample['type'], 1, -1) : ''; ?>
+<?php echo $apiDoc->apiSuccessExample['example']; ?>
+```
+<?php
+endif;
+// apiError
+if (!is_null($apiDoc->apiError)):
+    ?>
+
+###  Error return Parameter
+Key|Value Type|Description
+---|---|---
+<?php
+foreach ($apiDoc->apiError as $param):
+    $parsedParam = $apiDoc->parseField($param);
+    ?>
+    <?php
+    echo $parsedParam['key'];
+    ?>|<?php
+    echo $parsedParam['type'];
+    ?>|<?php
+    echo $parsedParam['description'];
+    ?>
+
+<?php
+endforeach;?>
+
+<?php
+endif;
+?>
+<?php
+// apiErrorExample
+if (!is_null($apiDoc->apiErrorExample)):
+    ?>
+### Example of a success return message
+<?php echo $apiDoc->apiErrorExample['title'] ?: ''; ?>
+
+```<?php echo isset($apiDoc->apiErrorExample['type']) ? substr($apiDoc->apiErrorExample['type'], 1, -1) : ''; ?>
+<?php echo $apiDoc->apiErrorExample['example']; ?>
 ```
 <?php
 endif;
